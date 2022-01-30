8275831fbd62ed08636bad53238d2d838d3de71e
Greg Wilson <greg@gbwilson.com>
2022-01-30 18:56:04 -0500

    Starting the challenge with starter code

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,59 +1,87 @@
-// Load sample 940 at the begining
-buildMetadata("940");
+function init() {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
 
-
-function optionChanged(newSample) {
-  buildMetadata(newSample);
-  // buildCharts(newSample);
-}
-
-function buildMetadata(sample) {
+  // Use the list of sample names to populate the select options
   d3.json("samples.json").then((data) => {
-    var metadata = data.metadata;
-    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-    var result = resultArray[0];
-    var PANEL = d3.select("#sample-metadata");
+    var sampleNames = data.names;
 
-  
-    
-  PANEL.html("");
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
 
-  console.log("++++++++++++++++++++++ Greg make a Fat Arrow Loop starting here +++++++++++++++++++++++");
-
-    ageText = "age:".toUpperCase().concat(result.age)
-    bbtypeText = "bbtype:".toUpperCase().concat(result.bbtype)
-    ethnicityText = "ethnicity:".toUpperCase().concat(result.ethnicity)
-    genderText = "gender:".toUpperCase().concat(result.gender)
-    idText = "id:".toUpperCase().concat(result.id)
-    locationText = "location:".toUpperCase().concat(result.location)
-    wfreqText = "wfreq:".toUpperCase().concat(result.wfreq)
-        
-    PANEL.append("h6").text(idText);
-    PANEL.append("h6").text(ethnicityText);
-    PANEL.append("h6").text(genderText);
-    PANEL.append("h6").text(ageText);
-    PANEL.append("h6").text(locationText);
-    PANEL.append("h6").text(bbtypeText);
-    PANEL.append("h6").text(wfreqText);
-
-  console.log("+++++++++++++++++++++ Greg make a Fat Arrow Loop ending here +++++++++++++++++++++++");
-    
+    // Use the first sample from the list to build the initial plots
+    var firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
   });
 }
 
-function init() {
-    var selector = d3.select("#selDataset");
+// Initialize the dashboard
+init();
+
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  buildMetadata(newSample);
+  buildCharts(newSample);
   
-    d3.json("samples.json").then((data) => {
-      console.log(data);
-      var sampleNames = data.names;
-      sampleNames.forEach((sample) => {
-        selector
-          .append("option")
-          .text(sample)
-          .property("value", sample);
-      });
-  })}
-  
-  init();
-  
+}
+
+// Demographics Panel 
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    // Filter the data for the object with the desired sample number
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    // Use d3 to select the panel with id of `#sample-metadata`
+    var PANEL = d3.select("#sample-metadata");
+
+    // Use `.html("") to clear any existing metadata
+    PANEL.html("");
+
+    // Use `Object.entries` to add each key and value pair to the panel
+    // Hint: Inside the loop, you will need to use d3 to append new
+    // tags for each key-value in the metadata.
+    Object.entries(result).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
+
+  });
+}
+
+// 1. Create the buildCharts function.
+function buildCharts(sample) {
+  // 2. Use d3.json to load and retrieve the samples.json file 
+  d3.json("samples.json").then((data) => {
+    // 3. Create a variable that holds the samples array. 
+
+    // 4. Create a variable that filters the samples for the object with the desired sample number.
+
+    //  5. Create a variable that holds the first sample in the array.
+
+
+    // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
+
+
+    // 7. Create the yticks for the bar chart.
+    // Hint: Get the the top 10 otu_ids and map them in descending order  
+    //  so the otu_ids with the most bacteria are last. 
+
+    var yticks = 
+
+    // 8. Create the trace for the bar chart. 
+    var barData = [
+      
+    ];
+    // 9. Create the layout for the bar chart. 
+    var barLayout = {
+     
+    };
+    // 10. Use Plotly to plot the data with the layout. 
+    
+  });
+}
